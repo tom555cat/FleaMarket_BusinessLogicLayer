fe6d34876ba9b70b9294be9931c788a1359f87e2
tom555cat <tongleiming1989@sina.com>
2016-03-21 18:02:46 +0800

    Initial Commit

---
+++ BusinessLogicLayer/BusinessLogicLayer.h
@@ -0,0 +1,13 @@
+//
+//  BusinessLogicLayer.h
+//  BusinessLogicLayer
+//
+//  Created by tom555cat on 16/3/21.
+//  Copyright © 2016年 H-T. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BusinessLogicLayer : NSObject
+
+@end
---
+++ BusinessLogicLayer/BusinessLogicLayer.m
@@ -0,0 +1,13 @@
+//
+//  BusinessLogicLayer.m
+//  BusinessLogicLayer
+//
+//  Created by tom555cat on 16/3/21.
+//  Copyright © 2016年 H-T. All rights reserved.
+//
+
+#import "BusinessLogicLayer.h"
+
+@implementation BusinessLogicLayer
+
+@end
